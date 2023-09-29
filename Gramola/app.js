ba0322b91b39dcb1786f9a73599c6f1985e1bed6
‘Alejandro <aespinoza@cendrassos.net>
2023-09-29 13:13:19 +0200

    Botones play, pausa, siguiente, anterior implementados, falta el boton parar.

--- Gramola/app.js
+++ Gramola/app.js
@@ -2,103 +2,214 @@ var playlistElement = document.getElementById("playlist");
 var cancionElement = document.getElementById("Canciones");
 var audioPlayer = document.getElementById("audioPlayer");
 var infoimg = document.getElementById("infoimg");
+var nextButton = document.getElementById("Siguiente");
+var beforeButton = document.getElementById("Anterior");
+var randomButton = document.getElementById("aleatorio");
+var playButton = document.getElementById("play");
+var imgChange = document.getElementById("imgchange");
+var currentPlaylistName = null; // Variable para almacenar el nombre de la lista de reproducción actual
+var currentIndex = 0; // Variable para almacenar el índice de la canción actual
+var songs = [];
+var max = 0; // Suponiendo que "songs" es tu array de canciones
+
+
+
 playlistElement.addEventListener("click", function(event) {
-    if (event.target && event.target.tagName === "A") {     // Si hacemos click al div  y no es a una playlist se borran las canciones.
-                                                            // Con esto comparamos el click si es un elemento html <a> se muestran las canciones
+    if (event.target && event.target.tagName === "A") {
+        event.preventDefault();
+        currentPlaylistName = event.target.textContent;
+        songs = cancionesData[currentPlaylistName];
+        currentIndex = 0;
+        cancionElement.innerHTML = "";
+        loadSongs();
+        console.log(songs)
+    }
+});
 
-        event.preventDefault();         // Evitamos que al hacer click a una playlist nos mande a otra pagina
 
-        var playlistName = event.target.textContent;    // Pilla el nombre de la lista de reproducción al hacer click en una de ellas
-        var songs = cancionesData[playlistName];        // conseguimos las canciones de la lista seleccionada
+function loadSongs() {
+    cancionElement.innerHTML = "";
+    songs.forEach(function(song) {
+        var songDiv = document.createElement("div");
+        songDiv.id = "Cancion";
+        var img = document.createElement("img");
+        img.src = song.image;
+        var infoDiv = document.createElement("div");
+        infoDiv.id = "info";
+        var tituloP = document.createElement("p");
+        tituloP.id = "titulo";
+        tituloP.textContent = song.Title;
+        var autorP = document.createElement("p");
+        autorP.id = "autor";
+        autorP.textContent = song.Artist;
+        var durationP = document.createElement("p");
+        durationP.id = "duration";
+        durationP.textContent = song.Duration;
 
-        cancionElement.innerHTML = "";          // Limpiamos el div Canciones para mostrar otras canciones de una playlist diferente
-        songs.forEach(function(song) {          // Crear y agregar elementos para cada canción
-
-            var songDiv = document.createElement("div");
-            songDiv.id = "Cancion";         // cada div lo creamos con este id
-
-            var img = document.createElement("img");
-            img.src = song.image;
-
-            var infoDiv = document.createElement("div");
-            infoDiv.id = "info";    //cada div lo creamos con este id
-
+        songDiv.addEventListener("click", function() {
+            nextButton.disabled = false;
+            currentIndex = songs.indexOf(song);
+            audioPlayer.src = song.Song;
+            audioPlayer.load();
+            audioPlayer.play();
+            imgChange.src = "./img/pause.png";
             var tituloP = document.createElement("p");
             tituloP.id = "titulo";
             tituloP.textContent = song.Title;
-
             var autorP = document.createElement("p");
             autorP.id = "autor";
             autorP.textContent = song.Artist;
-
-            var durationP = document.createElement("p");
-            durationP.id = "duration";
-            durationP.textContent = song.Duration;
-
-            songDiv.addEventListener("click", function() {
-        
-                audioPlayer.src = song.Song;       // Actualiza la fuente del elemento de audio para reproducir la canción
-                audioPlayer.load();
-                audioPlayer.play();         // Reproduce la canción al hacer clic
-
-                var tituloP = document.createElement("p");
-                tituloP.id = "titulo";
-                tituloP.textContent = song.Title;
-
-                var autorP = document.createElement("p");
-                autorP.id = "autor";
-                autorP.textContent = song.Artist;
-
-                var infoDiv = document.createElement("div");
-                infoDiv.id = "texto";
-
-                var img = document.createElement("img");
-                img.src = song.image;
-
-                infoDiv.appendChild(tituloP);
-                infoDiv.appendChild(autorP);
-                infoimg.innerHTML = ""; 
-                infoimg.appendChild(img);
-                infoimg.appendChild(infoDiv);
-                
-            });
-
-            infoDiv.appendChild(tituloP);          //hacemos que el titulo este debajo del div info
-            infoDiv.appendChild(autorP);           //hacemos que el autor este debajo del div info
-            
-
-            songDiv.appendChild(img);              //hacemos que el img este debajo del div cancion
-            songDiv.appendChild(infoDiv);          //hacemos que el div info este debajo del div cancion
-            songDiv.appendChild(durationP);
-
-            cancionElement.appendChild(songDiv);    // Añadimos el div Cancion como hijo del div Canciones para poder mostrar las canciones
+            var infoDiv = document.createElement("div");
+            infoDiv.id = "texto";
+            var img = document.createElement("img");
+            img.src = song.image;
+            infoDiv.appendChild(tituloP);
+            infoDiv.appendChild(autorP);
+            infoimg.innerHTML = "";
+            infoimg.appendChild(img);
+            infoimg.appendChild(infoDiv);
+            audioPlayer.addEventListener("ended", playNextSong);
         });
+
+        infoDiv.appendChild(tituloP);
+        infoDiv.appendChild(autorP);
+
+        songDiv.appendChild(img);
+        songDiv.appendChild(infoDiv);
+        songDiv.appendChild(durationP);
+
+        cancionElement.appendChild(songDiv);
+    });
+}
+
+
+function playNextSong() {
+    if (currentIndex +1 >= songs.length) {
+        console.log(currentIndex)
+        console.log(songs.length)
+        console.log("No hay más canciones disponibles.");
+        nextButton.disabled = true;
+    } else {
+        currentIndex++;
+        audioPlayer.src = songs[currentIndex].Song;
+        audioPlayer.load();
+        audioPlayer.play();
+
+        // Actualiza la información de la canción
+        var tituloP = document.createElement("p");
+            tituloP.id = "titulo";
+            tituloP.textContent = songs[currentIndex].Title
+            var autorP = document.createElement("p");
+            autorP.id = "autor";
+            autorP.textContent = songs[currentIndex].Artist;
+            var infoDiv = document.createElement("div");
+            infoDiv.id = "texto";
+            var img = document.createElement("img");
+            img.src = songs[currentIndex].image;;
+            infoDiv.appendChild(tituloP);
+            infoDiv.appendChild(autorP);
+            infoimg.innerHTML = "";
+            infoimg.appendChild(img);
+            infoimg.appendChild(infoDiv);
     }
-});
-        // BARRA DE DURACION
-        const progressBar = document.getElementById('progressBar');
-        const currentTime = document.getElementById('currentTime');
-        const duration = document.getElementById('duration');
+}
+function beforeSong() {
+    
+    if (currentIndex <=0) {
+         
+            beforeButton.disabled = true;
+            console.log("No hay más canciones para volver");
 
-        audioPlayer.addEventListener('timeupdate', function() {
-        const progress = (audioPlayer.currentTime / audioPlayer.duration) * 100;
-        progressBar.value = progress;
-        currentTime.textContent = formatTime(audioPlayer.currentTime);
-        });
+    } else {
+        currentIndex--;
+        console.log(currentIndex)
+        audioPlayer.src = songs[currentIndex].Song;
+        audioPlayer.load();
+        audioPlayer.play();
+        var tituloP = document.createElement("p");
+            tituloP.id = "titulo";
+            tituloP.textContent = songs[currentIndex].Title
+            var autorP = document.createElement("p");
+            autorP.id = "autor";
+            autorP.textContent = songs[currentIndex].Artist;
+            var infoDiv = document.createElement("div");
+            infoDiv.id = "texto";
+            var img = document.createElement("img");
+            img.src = songs[currentIndex].image;;
+            infoDiv.appendChild(tituloP);
+            infoDiv.appendChild(autorP);
+            infoimg.innerHTML = "";
+            infoimg.appendChild(img);
+            infoimg.appendChild(infoDiv);
+    }
+}
 
-        audioPlayer.addEventListener('loadedmetadata', function() {
+function randomsong() { 
+    max = songs.length
+
+    var randomIndex = Math.floor(Math.random() * max);
+    currentIndex = randomIndex
+    
+    if (currentIndex >= 0 & currentIndex < songs.length ) {
+        audioPlayer.src = songs[currentIndex].Song;
+        audioPlayer.load();
+        audioPlayer.play();
+        console.log(currentIndex +" -- "+songs[currentIndex].Title)
+        var tituloP = document.createElement("p");
+        tituloP.id = "titulo";
+        tituloP.textContent = songs[currentIndex].Title
+        var autorP = document.createElement("p");
+        autorP.id = "autor";
+        autorP.textContent = songs[currentIndex].Artist;
+        var infoDiv = document.createElement("div");
+        infoDiv.id = "texto";
+        var img = document.createElement("img");
+        img.src = songs[currentIndex].image;;
+        infoDiv.appendChild(tituloP);
+        infoDiv.appendChild(autorP);
+        infoimg.innerHTML = "";
+        infoimg.appendChild(img);
+        infoimg.appendChild(infoDiv); 
+    }
+}
+function playstopSong() {
+    if (audioPlayer.paused) {
+        audioPlayer.play();
+        imgChange.src = "./img/pause.png";
+    } else {
+        audioPlayer.pause();
+        imgChange.src = "./img/play.png";
+    }
+}
+
+playButton.addEventListener("click", playstopSong);
+randomButton.addEventListener("click", randomsong);
+beforeButton.addEventListener("click", beforeSong);
+nextButton.addEventListener("click", playNextSong);
+
+
+// BARRA DE DURACION
+    const progressBar = document.getElementById('progressBar');
+    const currentTime = document.getElementById('currentTime');
+    const duration = document.getElementById('duration');
+
+    audioPlayer.addEventListener('timeupdate', function() {
+    const progress = (audioPlayer.currentTime / audioPlayer.duration) * 100;
+    progressBar.value = progress;
+    currentTime.textContent = formatTime(audioPlayer.currentTime);
+    });
+    audioPlayer.addEventListener('loadedmetadata', function() {
         duration.textContent = formatTime(audioPlayer.duration);
-        });
-
-        function formatTime(time) {
+    });
+    function formatTime(time) {
         const minutes = Math.floor(time / 60).toString().padStart(2, '0');
         const seconds = Math.floor(time % 60).toString().padStart(2, '0');
         return `${minutes}:${seconds}`;
         }
-        progressBar.addEventListener('click', function(event) {
-            const progressBarWidth = progressBar.offsetWidth;
-            const clickX = event.offsetX;
-            const clickPercentage = (clickX / progressBarWidth) * 100;
-            const newTime = (clickPercentage / 100) * audioPlayer.duration;
-            audioPlayer.currentTime = newTime;
-          });
+    progressBar.addEventListener('click', function(event) {
+    const progressBarWidth = progressBar.offsetWidth;
+    const clickX = event.offsetX;
+    const clickPercentage = (clickX / progressBarWidth) * 100;
+    const newTime = (clickPercentage / 100) * audioPlayer.duration;
+    audioPlayer.currentTime = newTime;
+    });
